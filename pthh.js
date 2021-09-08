6b81e7634b7d96207a039d3f0798cd1769b8bc60
CuOIT <legendof2k2@gmail.com>
2021-09-08 13:18:56 +0700

    new

---
+++ pthh.js
@@ -0,0 +1,10 @@
+let input = document.querySelector("#input");
+let result = document.querySelector("#result");
+let a;
+input.addEventListener("change", (e)=>{
+    console.log(e.target.value);
+    result.innerText = e.target.value
+})
+function giaitich(){
+
+}
